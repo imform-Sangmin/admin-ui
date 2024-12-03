7fc3af4b99e0bd2ff0f4fa3ee28a0da740e3b829
안소현 <annsh@imform.co.kr>
2024-12-03 13:42:31 +0900

    feat: 레이아웃 table 세팅중

--- src/pages/app/splash/index.tsx
+++ src/pages/app/splash/index.tsx
@@ -18,6 +18,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useForm } from "react-hook-form";
 
 export default function AppSplash() {
@@ -33,72 +34,98 @@ export default function AppSplash() {
       </Title>
       <section className="px-56 flex flex-col gap-16">
         <Form {...form}>
-          <form>
-            <FormField
-              control={form.control}
-              name="search"
-              render={({ field }) => (
-                <div className="flex flex-col gap-16 w-full py-24 px-40 rounded-12 bg-white">
-                  <FormItem className="flex items-center w-full">
-                    <FormLabel className="w-121 flex-none">검색어</FormLabel>
-                    <FormControl>
-                      <div className="flex gap-8 w-full">
-                        <Select>
+          <form className="flex gap-8 w-full py-24 px-40 rounded-12 bg-white">
+            <div className="flex flex-col gap-16 flex-1">
+              <div className="flex items-center gap-8">
+                <FormLabel className="w-117 flex-none">검색어</FormLabel>
+                <FormField
+                  control={form.control}
+                  name="type"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Select {...field}>
                           <SelectTrigger className="w-160">
                             <SelectValue placeholder="이름" />
                           </SelectTrigger>
                           <SelectContent>
                             <SelectGroup>
-                              <SelectItem value="est">이름</SelectItem>
-                              <SelectItem value="cst">이름</SelectItem>
-                              <SelectItem value="mst">이름</SelectItem>
-                              <SelectItem value="pst">이름</SelectItem>
-                              <SelectItem value="akst">이름</SelectItem>
-                              <SelectItem value="hst">이름</SelectItem>
+                              <SelectItem value="name">이름</SelectItem>
+                              <SelectItem value="id">ID (이메일)</SelectItem>
+                              <SelectItem value="department">부서</SelectItem>
+                              <SelectItem value="authority">권한</SelectItem>
+                              <SelectItem value="registrationDate">
+                                등록일
+                              </SelectItem>
+                              <SelectItem value="accessTime">
+                                최근 접속시간
+                              </SelectItem>
                             </SelectGroup>
                           </SelectContent>
                         </Select>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="search"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormControl>
                         <Input
                           placeholder="검색어를 입력해주세요."
                           {...field}
                         />
-                        <div className="flex gap-4">
-                          <Button variant={"secondary1"}>
-                            <Icon type="search" />
-                            검색
-                          </Button>
-                          <Button variant={"secondary2"}>
-                            <Icon type="return" />
-                          </Button>
-                        </div>
-                      </div>
-                    </FormControl>
-                  </FormItem>
-                  <FormItem className="flex items-center h-40">
-                    <FormLabel className="w-121 flex-none">권한</FormLabel>
-                    <FormControl>
-                      <ul className="flex gap-24">
-                        <li className="flex items-center gap-6">
-                          <Checkbox id="total" />
-                          <label htmlFor="total">전체</label>
-                        </li>
-                        <li className="flex items-center gap-6">
-                          <Checkbox id="master" />
-                          <label htmlFor="master">최고관리자</label>
-                        </li>
-                        <li className="flex items-center gap-6">
-                          <Checkbox id="normal" />
-                          <label htmlFor="normal">일반관리자</label>
-                        </li>
-                      </ul>
-                    </FormControl>
-                  </FormItem>
-                </div>
-              )}
-            />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex items-center gap-8">
+                <FormLabel className="w-117 flex-none">권한</FormLabel>
+                <FormField
+                  control={form.control}
+                  name="authority"
+                  render={({ field }) => (
+                    <FormItem className="flex items-center h-40">
+                      <FormControl>
+                        <ul className="flex gap-24">
+                          <li className="flex items-center gap-6">
+                            <Checkbox id="total" />
+                            <label htmlFor="total">전체</label>
+                          </li>
+                          <li className="flex items-center gap-6">
+                            <Checkbox id="master" />
+                            <label htmlFor="master">최고관리자</label>
+                          </li>
+                          <li className="flex items-center gap-6">
+                            <Checkbox id="normal" />
+                            <label htmlFor="normal">일반관리자</label>
+                          </li>
+                        </ul>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <div className="flex gap-4">
+              <Button
+                variant={"secondary1"}
+                type="submit"
+                className="px-26 gap-4"
+              >
+                검색
+                <Icon type="search" />
+              </Button>
+              <Button variant={"secondary2"}>
+                <Icon type="return" />
+              </Button>
+            </div>
           </form>
         </Form>
-        <div className="py-20 rounded-12 bg-white">
+        <div className="flex flex-col gap-16 py-20 rounded-12 bg-white">
           <div className="ml-26 mr-40 flex justify-between items-end font-bold">
             <p>
               검색결과 <span className="text-states-red">00</span>건
@@ -138,6 +165,19 @@ export default function AppSplash() {
               </div>
             </div>
           </div>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>이름</TableHead>
+                <TableHead>ID (이메일)</TableHead>
+                <TableHead>부서</TableHead>
+                <TableHead>권한</TableHead>
+                <TableHead>등록일</TableHead>
+                <TableHead>최근 접속시간</TableHead>
+                <TableHead>수정</TableHead>
+              </TableRow>
+            </TableHeader>
+          </Table>
         </div>
       </section>
     </>
